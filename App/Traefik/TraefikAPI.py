85adf9a782dcbe3910cad3eac9b8d561208c12ef
Derek Vance <dvance@hom.org>
2019-09-16 15:01:26 -0400

    Added depends.

--- App/Traefik/TraefikAPI.py
+++ App/Traefik/TraefikAPI.py
@@ -4,16 +4,36 @@ from requests import Session
 
 class Traefik:
     def __init__(self, base_url: str, user: str = '', password: str = ''):
-        self.base_url = base_url.rstrip("/") + "/api"
+        self.base_urls = base_url.split(";")
+        self.base_url = ""
         self.session = Session()
 
         if len(user) > 0:
-            self._auth = (user, password,)
+            self.session.auth = (user, password,)
         else:
-            self._auth = None
+            self.session.auth = None
 
     def _get(self, method: str, params: dict = None):
-        return self.session.get(url="{}/{}".format(self.base_url, method), params=params, auth=self._auth)
+        retr = None
+        for base_url in self.base_urls:
+            base_url = base_url.lstrip("/") + "/api/"
+            try:
+                retr = self.session.get(url="{}/{}".format(base_url, method), params=params)
+                break
+            except:
+                pass
+        return retr
+
+    def _put(self, method: str, data):
+        retr = None
+        for base_url in self.base_urls:
+            base_url = base_url.lstrip("/") + "/api/"
+            try:
+                retr = self.session.put(url="{}/{}".format(base_url, method), data=data)
+                break
+            except:
+                pass
+        return retr
 
     def provider(self, provider: str = None) -> [dict, None]:
         method = "providers"
@@ -28,4 +48,4 @@ class Traefik:
     def put(self, provider: str = "rest", payload: dict = None):
         method = "providers/{}".format(provider)
         payload = json.dumps(payload)
-        return self.session.put(url="{}/{}".format(self.base_url, method), data=payload, auth=self._auth)
+        return self._put(method, data=payload)
