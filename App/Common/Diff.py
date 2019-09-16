36dd50fec8c16970a056de39d417f05a02200c11
Derek Vance <DACRepair@gmail.com>
2019-09-16 17:57:15 -0400

    oops

--- App/Common/Diff.py
+++ App/Common/Diff.py
@@ -2,5 +2,5 @@ import json
 
 
 def dict_diff(new, exist):
-    exist = exist[exist.keys[0]]
+    exist = exist[exist.keys()[0]]
     return json.dumps(new, sort_keys=True) == json.dumps(exist, sort_keys=True)
