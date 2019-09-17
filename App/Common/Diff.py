f86dd1e9f9d98a2afbf20c7001ddeac4f9b9c02e
Derek Vance <DACRepair@gmail.com>
2019-09-16 20:24:51 -0400

    Fixed issue where data diff gets an empty result.

--- App/Common/Diff.py
+++ App/Common/Diff.py
@@ -2,5 +2,8 @@ import json
 
 
 def dict_diff(new, exist):
-    exist = exist[list(exist.keys())[0]]
-    return json.dumps(new, sort_keys=True) == json.dumps(exist, sort_keys=True)
+    if len(exist) == 0 or len(new):
+        return False
+    else:
+        exist = exist[list(exist.keys())[0]]
+        return json.dumps(new, sort_keys=True) == json.dumps(exist, sort_keys=True)
